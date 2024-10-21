b8e23675d20b5b0c78a3316e9afb62e940158b56
ahmsolt <ahm.solt@gmail.com>
2024-10-21 23:05:46 +0330

    1403.06.30
    1403.06.30

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
+    
