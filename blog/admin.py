72fe086ec7eabbba47cf533de0d47e736ea08bf4
ahmsolt <157969838+ahmsolt@users.noreply.github.com>
2024-10-22 09:49:10 +0330

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Post,Category
 
 # Register your models here.
+
+class PostAdmin(admin.modelAdmin):
+    list_display = ('title','author','status')
+    list_filter = ('status',)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
