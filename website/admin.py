16d26c506e062239c8fc663dd03b542cca11f91a
ahmsolt <157969838+ahmsolt@users.noreply.github.com>
2024-10-23 15:58:51 +0330

    contact
    office model and admin in contact

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','email')
+
+admin.site.register(Contact,ContactAdmin)
