608ffa2c486147c67ec556db6b5ffc65f589665b
ahmsolt <157969838+ahmsolt@users.noreply.github.com>
2024-10-23 18:17:48 +0330

    blog single

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
 from blog.models import Post
 
 def blog_view(request):
-    return render (request,'blog/blog-home.html')
-def blog_single(request):
-    return render (request,'blog/blog-single.html')
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render (request,'blog/blog-home.html',context)
+def blog_single(request,pid):
+    post = get_list_or_404(Post,pk=pid)
+    context = {'post':post}
+    return render (request,'blog/blog-single.html',context)
