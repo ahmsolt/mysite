83879cc04c42abe3c65852f0ff35a2ac77916433
ahmsolt <157969838+ahmsolt@users.noreply.github.com>
2024-10-30 17:33:33 +0330

    نه آبان هفده و سی

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,7 +2,11 @@ from django import template
 from blog.models import Post
 register = template.Library()
 
-@register.simple_tag(name='totalposts')
+@register.simple_tag(name='plustwo')
+def function(a=5):
+    return a + 2
+
+@register.simple_tag(name='posts')
 def function():
     posts = Post.objects.filter(status=1)
     return posts
