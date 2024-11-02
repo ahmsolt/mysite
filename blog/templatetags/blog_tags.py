5c9eb640be96a93eeb66b688463acbef0aeb1daa
ahmsolt <157969838+ahmsolt@users.noreply.github.com>
2024-11-02 14:42:57 +0330

    12 آبان

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -16,7 +16,7 @@ def function():
 def snippet(value,arg=20):
     return value[:arg] + "..."
 
-@register.inclusion_tag('blog/popular-posts.html')
+@register.inclusion_tag('blog/blog-popular-posts.html')
 def latestposts():
-    posts = Post.objects.filter(status=1).order_by('Published_date')[:1]
+    posts = Post.objects.filter(status=1).order_by('Published_date')[:3]
     return {'posts':posts}
