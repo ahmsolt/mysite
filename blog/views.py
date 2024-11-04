117641a768c7e02c2f2ead590bcae99e160b78cb
ahmsolt <157969838+ahmsolt@users.noreply.github.com>
2024-11-04 16:44:19 +0330

    end of 7

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
-def blog_view(request,cat_name=None):
+def blog_view(request,cat_name=None,author_username=None):
     posts = Post.objects.filter(status=1)
     if cat_name:
         posts = posts.filter(category__name=cat_name)
+    if author_username:
+        posts = posts.filter(author__username = author_username)
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
@@ -22,5 +33,16 @@ def test(request):
 def blog_category(request,cat_name):
     posts = Post.objects.filter(status=1)
     posts = posts.filter(Category__name = cat_name)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
+
+def blog_search(request):
+    #print(request.__dict__)
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        #print(request.GET.get('s'))
+        if s := request.GET.get('s'):
+             posts = posts.filter(content__contains=s)
+    
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
