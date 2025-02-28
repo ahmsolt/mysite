f32c370c505454903917fe0d552d754eb9a8a745
ahmsolt <ahm.solt@gmail.com>
2025-02-28 21:30:42 +0330

    10 اسفند

--- website/views.py
+++ website/views.py
@@ -1,8 +1,30 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from website.models import Contact  
+from website.forms import NameForm,ContactForm,NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render (request,'website/index.html')
 def about_view(request):
     return render (request,'website/about.html')
 def contact_view(request):
-    return render (request,'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'yedone ')
+        else:
+            messages.add_message(request,messages.ERROR,'ye!!!!!!done ')
+
+    form = ContactForm()
+    return render(request,'website/contact.html',{'form':form})
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            
+            
